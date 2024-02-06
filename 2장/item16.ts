35b5d9c926499df9dbd47d3319cab07da935c859
dongwoo.seo <dongwoo.seo@navercorp.com>
2024-02-06 18:42:50 +0900

    16장 정리

---
+++ 2장/item16.ts
@@ -0,0 +1,31 @@
+"0" == 0
+const x = {};
+x[[1,2,3]]=2;
+
+const a = {1:2, 3:4};
+// 자바스크립트는 배열이라는 개념이 없음
+const x = [1,2,3];
+
+Array;
+
+const xs = [1,2,3];
+const x0 = xs[0];
+const x1 = xs['1'];
+
+function get<T>(array: T[], k: string): T {
+    return array[k];
+}
+
+const keys = Object.keys(xs);
+for (const key in xs) {
+    key;
+    const x = xs[key];
+}
+
+
+
+
+
+
+
+export {};
