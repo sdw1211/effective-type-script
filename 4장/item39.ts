a8c39265da74ac44bde240385546d01007fab6e0
dongwoo.seo <dongwoo.seo@navercorp.com>
2024-03-14 05:39:09 +0900

    아이템39,40 추가

--- 4장/item39.ts
+++ 4장/item39.ts
@@ -1,1 +1,62 @@
+// 아이템39 any를 구체적으로 변형해서 사용하기
+
+// any 자바스크립트에서 표현할 수 있는 모든 값을 아우르는 매우 큰 범위의 타입
+// any를 사용하는 것보다 더 구체적인 타입을 찾아 타입 안전성을 높이도록 노력하자
+
+// 배열인 경우
+// bad
+function getLengthBad(array: any) {
+    return array.length;
+
+}
+
+getLengthBad(1111);
+
+
+// good
+function getLengthGood(array: any[]) {
+    return array.length;
+}
+
+getLengthGood(1111);
+
+
+// 함수 내의 array.length 타입이 체크
+// 함수의 반환 타입이 any 대신 number 로 추론
+// 함수 호출될 때 매개변수가 배열인지 체크
+
+// 객체인 경우
+// bad
+function hasTweleveLetterKey(o: any) {
+    for (const key in o) {
+        if(key.length == 12) {
+            console.log(key, o[key]);
+            return true;
+        }
+    }
+    return false;
+}
+
+function hasTweleveLetterKey2(o: {[key: string]: any}) {
+    for (const key in o) {
+        if(key.length == 12) {
+            console.log(key, o[key]);
+            return true;
+        }
+    }
+    return false;
+}
+
+// 함수의 경우
+
+type FnAny = any;
+
+type Fn0 = () => any;
+type Fn1 = (a: any) => any;
+type Fn2 = (a: any, b: any) => any;
+
+const numArgsBad = (...args: any) => args.length;
+const numArgsGood = (...args: any[]) => args.length;
+
+
 export {};
--- 4장/item40.ts
+++ 4장/item40.ts
@@ -1,1 +1,62 @@
+// 아이템40 함수 안으로 타입 단언문 감추기
+
+// 함수를 작성하다 보면, 외부로 드로난 타입 정의는 간단하지만 내부 로직이 복잡해서 안전한 타입으로 구현하기 어려운 경우가 많다.
+// 함수의 모든 부분을 안전한 타입으로 구현하는 것이 이상적이지만, 불필요한 예외 상황까지 고려해 가며 타입 정보를 힘들게 구성할 필요는 없다.
+
+declare function cacheLast<T extends Function>(fn: T): T; 
+declare function shallowEqual(a: any, b: any): boolean;
+
+function cacheLast1<T extends Function>(fn: T): T {
+    let lastArgs: any[] | null = null;
+    let lastResult: any;
+    let c = () => {};
+
+    return function(...args: any[]) {
+        if (!lastArgs || !shallowEqual(lastArgs, args)) {
+            lastResult = fn(...args);
+            lastArgs = args;
+        }
+        return lastResult;
+    } as unknown as T;
+}
+
+class B {
+    a: number;
+    constructor(a: number) {
+        this.a = a;
+    }
+}
+class A extends B {
+    b: number;
+    constructor(b: number) {
+        super(b);
+        this.b = b;
+    }
+
+}
+
+const a:A = new A(10);
+const b:B = new A(10);
+
+const c:A = new B(10);
+
+const c3:A = b;
+const c2:A = b as A;
+
+// https://www.typescriptlang.org/ko/docs/handbook/2/everyday-types.html#type-assertions
+cacheLast1(() => {})();
+
+function shallowObjectEqual<T extends object>(a: T, b: T): boolean {
+    for (const [k, aval] of Object.entries(a)) {
+        if(!(k in b) || aval !== (b as any)[k]) {
+            return false;
+        }
+    }
+
+    return Object.keys(a).length === Object.keys(b).length;
+}
+
+
+
+
 export {};
